12914bdf92fb1baa0a1de97ab4c1824607d3d95c
Michael Dempsey <michaelstdempsey@gmail.com>
2019-02-18 00:51:20 -0500

    Added wind runway dir controller

--- wind_app.py
+++ wind_app.py
@@ -18,17 +18,23 @@ class WindCalcView(ui.View):
     def __init__(self):
         self.wind_calculator = winds.WindCalculator()
         self.wind_dir, self.wind_speed = 0, 0
+        self._last_wind_dir_slider_val = .5
+        self._last_runway_slider_val = .5
 
     
     def did_load(self):
         self.wind_info_label = self['wind_info_label']
         self.wind_dir_slider = self['wind_dir_slider']
+        self.wind_dir_slider.continuous = True
         self.wind_dir_slider.action = self.dir_slider_moved
         self.wind_speed_slider = self['wind_speed_slider']
         self.wind_speed_slider.action = self.wind_speed_slider_moved
+        self.wind_speed_slider.continuous = True
         self.runway_dir_label = self['runway_dir_label']
         
         self.runway_wind_controller = self['runway_wind_controller']
+        self.runway_wind_controller.action = self.snap_slider_to_prev_val
+
         
         self.update_wind_info_label()
         self.update_runway_dir_label()
@@ -37,8 +43,17 @@ class WindCalcView(ui.View):
         kwargs = {'wind_dir': f'{self.wind_dir:.1f}', 'wind_speed': f'{self.wind_speed:.1f}'}
         self.wind_info_label.text = self.WIND_INFO_LABEL_TEMPLATE.safe_substitute(**kwargs)
         
-    def snap_slider_to_val(self):
-        pass
+    def snap_slider_to_prev_val(self, wind_rwy):
+        val = wind_rwy.segments[wind_rwy.selected_index]
+        prev = 'WIND' if val == 'RWY' else 'RWY'
+        slider_val = self.wind_dir_slider.value
+        if prev == 'WIND':
+            self._last_wind_dir_slider_val = slider_val
+            self.wind_dir_slider.value = self._last_runway_slider_val
+        else:
+            self._last_runway_slider_val = slider_val
+            self.wind_dir_slider.value = self._last_wind_dir_slider_val
+
         
     def update_runway_dir_label(self):
         val = self.wind_calculator.runway_heading
@@ -61,7 +76,8 @@ class WindCalcView(ui.View):
         func = {
             'RWY': update_runway,
             'WIND': update_wind,
-        }.get(self.runway_wind_controller.segments[self.runway_wind_controller.selected_index])(sender)
+        }.get(self.runway_wind_controller.segments[self.runway_wind_controller.selected_index])
+        func(sender)
         
         
     def wind_speed_slider_moved(self, sender):
@@ -78,7 +94,7 @@ if __name__ == '__main__':
     
     wind_calc_view = view['wind_calc_view']
 
-    view.present()
+    view.present('fullscreen')
 
     
     
